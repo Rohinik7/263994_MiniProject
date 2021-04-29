f015a48b514a4cce7cdb4eb1dbb3c455ea48a525
Rohini Nair <59047425+Rohinik7@users.noreply.github.com>
2021-04-29 20:38:43 +0530

    Hangman game to guess movies

--- hangman_code.py
+++ hangman_code.py
@@ -13,7 +13,7 @@ import string
 
 def get_txt():
     words = {}
-    movie_names=open('words.txt','r')       # get the movie names from the file words.txt
+    movie_names = open('words.txt','r')       # get the movie names from the file words.txt
     for line in movie_names:
         line=line.strip("\n")
         lst=list(map(str,line.split(":")))
@@ -24,18 +24,18 @@ def get_txt():
 
 def add_txt(movie,director):
     movie_names = open('words.txt', 'a')  # write movie name to the file words.txt
-    line="\n{}:{}".format(movie,director)
+    line = "\n{}:{}".format(movie,director)
     movie_names.write(line)
 
 
-def get_valid_word():
-    words=get_txt()     # calls function get_txt
+def get_valid_word(words):
     word = random.choice(list(words.keys()))  # randomly chooses something from the list
     return word.upper(),words[word]  # return word and the hint word(director)
 
 
 def initialize():
-    word , Hint= get_valid_word()         # get_valid_word function called
+    words = get_txt()
+    word , Hint= get_valid_word(words)
     word_letters = set(word)  # letters in the word
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
@@ -57,14 +57,13 @@ def display(word_letters,used_letters,Hint,lives,word):
     # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
     print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
     # what current word is (ie W - R D)
-    used_letters,word_letters=format_input(word_letters,used_letters)                # format_input function called
+    used_letters,word_letters=format_input(word_letters,used_letters)
     word_list = [letter if letter in used_letters else '-' for letter in word]
-    print(lives_left[lives])
     print('Current word: ', ' '.join(word_list))
     print("Hint: Movie Director: ", Hint)
 
 
-def user_input(word_letters,lives):
+def user_input():
     # Take in user input
     user_letter = input('Guess a letter: ').upper()
     return user_letter
@@ -78,7 +77,8 @@ def update(user_letter,used_letters,word_letters,alphabet,lives):
             word_letters.remove(user_letter)
             print('')
         else:
-            lives = lives - 1      # takes away a life if wrong
+            lives = lives - 1  # takes away a life if wrong
+            print(lives_left[lives])
             print('\nYour letter,', user_letter, 'is not in the word.')
     elif user_letter in used_letters:
         print('\nYou have already used that letter. Guess another letter.')
@@ -91,29 +91,29 @@ def end(lives,word):
     # gets here when len(word_letters) == 0 OR when lives == 0
     if lives == 0:
         print(lives_left[lives])
-        print('You died sorry, better luck next time! . The Movie was', word)
+        print('You died sorry, better luck next time! The Movie was', word)
     else:
         print('YAY! You guessed the Movie', word, '!!')
 
 
 def user_contribution():
-    inp=input("Would you like to add movie ? (y/n)")
-    if inp.lower()=="y":
-        movie=input("Movie name: ")
-        director=input("Director: ")
+    inp = input("Would you like to add movie ? (y/n)")
+    if inp.lower() == "y":
+        movie = input("Movie name: ")
+        director = input("Director: ")
         add_txt(movie,director)          # call function add_txt to write movie to file
     else:
         exit()
 
 
-def hangman():    # to call functions
+def hangman():         # call functions
     word, Hint , word_letters, alphabet, used_letters, lives = initialize()
     while len(word_letters) > 0 and lives > 0:
-        display(word_letters, used_letters, Hint, lives, word)      # display function called
-        user_letter = user_input(word_letters, lives)           # user_input function called
-        used_letters,lives = update(user_letter, used_letters, word_letters, alphabet, lives)   # update function called
-    end(lives, word)                  # end function called
-    user_contribution()        # user_contribution function called
+        display(word_letters, used_letters, Hint, lives, word)
+        user_letter = user_input()
+        used_letters,lives = update(user_letter, used_letters, word_letters, alphabet, lives)
+    end(lives, word)
+    user_contribution()
 
 
 if __name__ == '__main__':
---
+++ test_hangman_code.py
@@ -0,0 +1,44 @@
+"""Pytest for hangman_code"""
+
+from unittest import TestCase
+
+import hangman_code as h
+import random
+import string
+
+
+def test_format_input():
+    word_letters = {'A', 'B', 'C', ' ', 'D'}
+    used_letters = {'U', 'S', 'E', 'D'}
+    used_letters, word_letters = h.format_input(word_letters, used_letters)
+    assert word_letters == {'A', 'B', 'C', 'D'} and used_letters == {' ', 'D', 'E', 'S', 'U'}
+
+
+def test_get_valid_word():
+    words = h.get_txt()
+    random.seed(53)
+    word, hint = h.get_valid_word(words)
+    assert word == "JOKER" and hint == "Todd Phillips"
+
+
+def test_update1():
+    user_letter = "E"
+    alphabet = set(string.ascii_uppercase)
+    word_letters = {'U', 'S', 'E', 'D'}
+    used_letters = {'A', 'B', 'C', 'D'}
+    lives = 1
+    used_letters, lives = h.update(user_letter, used_letters, word_letters, alphabet, lives)
+    assert used_letters == {'A', 'B', 'C', 'D', 'E'} and word_letters == {'U', 'S', 'D'}
+
+
+def test_update2():
+    user_letter = "W"
+    alphabet = set(string.ascii_uppercase)
+    word_letters = {'U', 'S', 'E', 'D'}
+    used_letters = {'A', 'B', 'C', 'D'}
+    lives = 1
+    used_letters, lives = h.update(user_letter, used_letters, word_letters, alphabet, lives)
+    assert lives == 0
+
+
+
